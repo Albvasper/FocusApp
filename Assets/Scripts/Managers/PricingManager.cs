a32cc8da1854f32611d66419eaee38866aa3b1e3
Alberto Vasquez <albert.vp09@gmail.com>
2026-05-06 22:07:32 -0600

    refactor: first version of pricing system

--- Assets/Scripts/Managers/PricingManager.cs
+++ Assets/Scripts/Managers/PricingManager.cs
@@ -1,19 +1,109 @@
 using UnityEngine;
 
-public class PricingManager : MonoBehaviour
+public class PricingManager : Purchases.UpdatedCustomerInfoListener
 {
     [SerializeField] private PricingManagerUI pricingManagerUI;
 
     public bool IsSubscribedToPomePlus { get; private set;}
+    public Purchases Purchases { get; private set; }
+
+    private void Awake() 
+    {
+        Purchases = GetComponent<Purchases>();
+        DontDestroyOnLoad(gameObject);
+    }
 
     private void Start() 
     {
-        // Check if subsribed to pome+
-        IsSubscribedToPomePlus = false;
+        Purchases.SetDebugLogsEnabled(true);
+        Purchases.GetCustomerInfo((customerInfo, error) =>
+        {
+            if (error != null) return;
+            CheckAccess(customerInfo);
+        });
+
+        Purchases.GetOfferings((offerings, error) =>
+        {
+            if (error != null)
+            {
+                // show error
+            }
+            else
+            {
+                if (offerings.Current == null) return;
+
+                foreach (var package in offerings.Current.AvailablePackages)
+                {
+                    // Show offerings in pricing screen
+                    // if (package.PackageType == Purchases.PackageType.Monthly)
+                    // {
+                    //     monthlyPriceText.text = package.StoreProduct.PriceString; // e.g. "$3.99/month"
+                    //     monthlyButton.onClick.AddListener(() => BeginPurchase(package));
+                    // }
+                    // else if (package.PackageType == Purchases.PackageType.Annual)
+                    // {
+                    //     yearlyPriceText.text = package.StoreProduct.PriceString; // e.g. "$29.99/year"
+                    //     yearlyButton.onClick.AddListener(() => BeginPurchase(package));
+                    // }
+                }
+            }
+        });
     }
 
-    public void SubscribeToPomePlus()
+    public override void CustomerInfoReceived(Purchases.CustomerInfo customerInfo)
     {
-        IsSubscribedToPomePlus = true;
+        CheckAccess(customerInfo);
+    }
+
+    public void BeginPurchase(Purchases.Package package)
+    {
+        Purchases.PurchasePackage(package, (purchaseResult) =>
+        {
+            if (purchaseResult.Error != null)
+            {
+                Debug.LogError("Purchase error: " + purchaseResult.Error.Message);
+                pricingManagerUI.ShowErrorScreen();
+            }
+            else if (purchaseResult.UserCancelled)
+            {
+                // User cancelled, dont show error
+                pricingManagerUI.HidePricingScreen();
+            }
+            else
+            {
+                CheckAccess(purchaseResult.CustomerInfo);
+                pricingManagerUI.ShowPurchaseSuccessfulScreen();
+            }
+        });
+    }
+
+    public void RestorePruchases()
+    {
+        Purchases.RestorePurchases((customerInfo, error) =>
+        {
+            if (error != null)
+            {
+                pricingManagerUI.ShowErrorScreen();
+            }
+            else
+            {
+                CheckAccess(customerInfo);
+                pricingManagerUI.ShowPurchaseRestoredScreen();
+            }
+        });
+    }
+
+    private void CheckAccess(Purchases.CustomerInfo customerInfo)
+    {
+        if (customerInfo.Entitlements.Active.ContainsKey("pome_plus"))
+        {
+            Debug.Log("Pome+ active");
+            IsSubscribedToPomePlus = true;
+        }
+        else
+        {
+            Debug.Log("Pome+ not active");
+            IsSubscribedToPomePlus = false;
+        }
     }
 }
--- Assets/Scripts/Managers/UI/PricingManagerUI.cs
+++ Assets/Scripts/Managers/UI/PricingManagerUI.cs
@@ -1,14 +1,41 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PricingManagerUI : MonoBehaviour
 {   
-    [SerializeField] private PricingManager pricingManager;
+    [Header("Screens")]
     [SerializeField] private GameObject pricingScreen;
+    [SerializeField] private GameObject errorMessageScreen;
+    [SerializeField] private GameObject purchaseSuccessfulScreen;
+    [SerializeField] private GameObject purchaseRestoredScreen;
+    [Header("Components")]
     [SerializeField] private Toggle monthlyPlanToggle;
     [SerializeField] private Toggle annuallyPlanToggle;
+    [Header("Manager")]
+    [SerializeField] private PricingManager pricingManager;
 
     private bool monthlyPlanSelected;
+    private Purchases.Package monthlyPackage;
+    private Purchases.Package yearlyPackage;
+
+    private void OnEnable() 
+    {
+        /*
+        pricingManager.Purchases.GetOfferings((offerings, error) =>
+        {
+            if (error != null || offerings.Current == null) return;
+
+            foreach (var package in offerings.Current.AvailablePackages)
+            {
+                if (package.PackageType == Purchases.PackageType.Monthly)
+                    monthlyPackage = package;
+                else if (package.PackageType == Purchases.PackageType.Annual)
+                    yearlyPackage = package;
+            }
+        });
+        */
+    }
 
     public void Start()
     {
@@ -24,6 +51,39 @@ public class PricingManagerUI : MonoBehaviour
     public void HidePricingScreen()
     {
         pricingScreen.SetActive(false);
+    }
+
+    public void ShowErrorScreen()
+    {
+        HidePricingScreen();
+        errorMessageScreen.SetActive(true);
+    }
+
+    public void HideErrorScreen()
+    {
+        errorMessageScreen.SetActive(false);
+    }
+
+    public void ShowPurchaseRestoredScreen()
+    {
+        HidePricingScreen();
+        purchaseRestoredScreen.SetActive(true);
+    }
+
+    public void HidePurchaseRestoredScreen()
+    {
+        purchaseRestoredScreen.SetActive(false);
+    }
+
+    public void ShowPurchaseSuccessfulScreen()
+    {
+        HidePricingScreen();
+        purchaseSuccessfulScreen.SetActive(true);
+    }
+
+    public void HidePurchaseSuccessfulScreen()
+    {
+        purchaseSuccessfulScreen.SetActive(false);
     }
 
     public void SelectMonthlyPlan()
@@ -42,13 +102,21 @@ public class PricingManagerUI : MonoBehaviour
 
     public void GoToCheckout()
     {
-        //TODO: Subscription
-        HidePricingScreen();
-        pricingManager.SubscribeToPomePlus();
+        if (monthlyPlanSelected)
+        {
+            if (monthlyPackage == null) return;
+            pricingManager.BeginPurchase(monthlyPackage);
+        }
+        else
+        {
+            if (yearlyPackage == null) return;
+            pricingManager.BeginPurchase(yearlyPackage);
+        }
+
     }
 
-    public void Cancel()
+    public void RestorePruchases()
     {
-        HidePricingScreen();
+        pricingManager.RestorePruchases();
     }
 }
